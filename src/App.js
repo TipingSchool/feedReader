0587d7ba3835b15c07cec5fcdb2a4fa72d6ff3a1
Suraj Prakash <surajprakash094@gmail.com>
2017-09-11 00:22:41 +0530

    indexpage1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import mongo from './mongo.jpg';
 import './App.css';
 
+
+const CatCard = (props) =>{
+  return(
+    <div>
+      <div className='card-box'>
+            <img src={props.imgsrc} className='card-img'/>
+            <p> {props.name} </p>
+            </div>
+      </div>
+  )
+}
+
 class App extends Component {
+        constructor(props){
+          super(props);
+          this.state ={
+            CardValue :[{'imgsrc':mongo, 'name':'MongoDB'},{'imgsrc':mongo, 'name':'NodeJS'},{'imgsrc':mongo, 'name':'NodeJS'},
+            {'imgsrc':mongo, 'name':'NodeJS'},{'imgsrc':mongo, 'name':'NodeJS'}]
+          }
+        }
+
   render() {
     return (
       <div className="App">
-        <div className="App-heade">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="App-header">
+          <nav>
+          <span className='nav-span'>Feeds</span>
+          <span className='nav-span'>Login</span>
+          <input type='text' value='search' />
+          </nav>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='flex-container'>
+         {this.state.CardValue.map((value) => <CatCard {...value}/>)}
+
+          </div>
       </div>
     );
   }
