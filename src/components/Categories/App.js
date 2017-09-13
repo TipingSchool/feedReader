dd2ca4c3d27308e9fc4c91f36dc347a9ef8c5bbd
Suraj Prakash <surajprakash094@gmail.com>
2017-09-13 12:32:38 +0530

    reshuffling file structure

--- src/components/Categories/App.js
+++ src/components/Categories/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import mongo from './mongo.jpg';
+import node from './node.png';
+import react from './react.png';
+import webdev from './webdep.jpg';
+import devop from './devops.jpg';
+import html from './html.jpg';
 import './App.css';
 
 
@@ -9,16 +14,18 @@ class CatCard extends Component{
   
   }
   ClickHandler = () =>{
-    alert('you clicked' + this.props.name);
+    alert('you clicked ' + this.props.name);
   }
 
   render(){
     return(
     <div>
       <div className='card-box' onClick = {this.ClickHandler}>
+        <div>
             <img src={this.props.imgsrc} className='card-img' alt={this.props.name}/>
             <p> {this.props.name} </p>
             </div>
+        </div>
       </div>
     );
   }
@@ -28,8 +35,8 @@ class App extends Component {
         constructor(props){
           super(props);
           this.state ={
-            CardValue :[{'imgsrc':mongo, 'name':'MongoDB'},{'imgsrc':mongo, 'name':'NodeJS'},{'imgsrc':mongo, 'name':'NodeJS'},
-            {'imgsrc':mongo, 'name':'NodeJS'},{'imgsrc':mongo, 'name':'NodeJS'},{'imgsrc':mongo, 'name':'NodeJS'}]
+            CardValue :[{'imgsrc':mongo, 'name':'MongoDB'},{'imgsrc':node, 'name':'NodeJS'},{'imgsrc':react, 'name':'ReactJS'},
+            {'imgsrc':webdev, 'name':'Web Development'},{'imgsrc':devop, 'name':'DevOps'},{'imgsrc':html, 'name':'HTML5'}]
           }
        
         }
@@ -45,6 +52,8 @@ class App extends Component {
             <input type='text' placeholder ='search'/>
           </nav>
         </div>
+        <div className="stat">
+          </div>
         <div className='flex-container' >
          {this.state.CardValue.map((value,i) =>(  <CatCard key={i} {...value}  />))}
         </div>
