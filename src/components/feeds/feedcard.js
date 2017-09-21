545f495c01f2a72e531e6c00cfee67337271e17e
slayercoder <ms81291@gmail.com>
2017-09-21 12:01:52 +0530

    minor changes which I dont remember

--- src/components/feeds/feedcard.js
+++ src/components/feeds/feedcard.js
@@ -9,33 +9,48 @@ const checkboxStyle = {
   fontSize : "15px"
 }
 
-const checkbox_text = {
-  paddingBottom : "5px",
-  marginBottom : "10px"
-};
+// const checkbox_text = {
+//   paddingBottom : "5px",
+//   marginBottom : "10px"
+// };
 
-const FeedCard = (props) => (
-    <div>
+// const FeedCard = (props) => (
+class FeedCard extends Component{    
+  constructor(props){
+    super(props);
+      this.state = {
+        title : this.props.title
+      }
+    }
+
+    handleCheckChange = () => {
+    }
+  
+  render(){
+    return(
+      <div>
         <div className='feed-card-box'>
           <div className='img-card col-md-2'>
             <img className='feed-card-img' src={ node1 } alt='try'/>
           </div>
           <div className='description-box col-md-10'>
-            <p className='heading'> { props.title }</p>
+            <p className='heading'> { this.props.title }</p>
             <div>
-              <author> { props.creator }</author>
-              <date>{ new Date(props.date).toDateString() }</date>
+              <author> { this.props.creator }</author>
+              <date>{ new Date(this.props.date).toDateString() }</date>
             </div>
             <div className='description'>
-              { props.description.substring(0,150) + '...' }
+              { this.props.description.substring(0,150) + '...' }
             </div>
           </div>
         </div>
         <div style = { checkboxStyle }>
-            <input type = "checkbox" name = "select_feed" value = { props.title }/><span style = { checkbox_text }></span><br/>
+            <input type = "checkbox" name = "select_feed" value = { this.state.title } onChange = { this.handleCheckChange }/><br/>
         </div>
     </div>
-);
-
+    );
+        
+  }
+}
 
 export default FeedCard;
